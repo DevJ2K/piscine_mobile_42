333565267915d03fadc89f88e8e09d5b242f27e2
theo <ajavontheopro@gmail.com>
2024-04-17 21:49:06 +0200

    hiding google and github login

--- Module04/diaryapp/diaryapp/Views/LoginView.swift
+++ Module04/diaryapp/diaryapp/Views/LoginView.swift
@@ -13,25 +13,25 @@ struct LoginView: View {
     var body: some View {
         Text("Welcome to your Diary")
             .font(.title)
-//        HStack {
-//            Button("Login", action: self.login)
-//                .padding()
-//                .background(.green)
-//                .foregroundStyle(.white)
-//                .clipShape(RoundedRectangle(cornerRadius: 12))
-//        }
         HStack {
-            Button("Google", action: self.loginWithGoogle)
-                .padding()
-                .background(.green)
-                .foregroundStyle(.white)
-                .clipShape(RoundedRectangle(cornerRadius: 12))
-            Button("GitHub", action: self.loginWithGithub)
+            Button("Login", action: self.login)
                 .padding()
                 .background(.green)
                 .foregroundStyle(.white)
                 .clipShape(RoundedRectangle(cornerRadius: 12))
         }
+//        HStack {
+//            Button("Google", action: self.loginWithGoogle)
+//                .padding()
+//                .background(.green)
+//                .foregroundStyle(.white)
+//                .clipShape(RoundedRectangle(cornerRadius: 12))
+//            Button("GitHub", action: self.loginWithGithub)
+//                .padding()
+//                .background(.green)
+//                .foregroundStyle(.white)
+//                .clipShape(RoundedRectangle(cornerRadius: 12))
+//        }
     }
 }
 
@@ -42,34 +42,34 @@ extension LoginView {
         //        print(jwt["email"].string)
     }
     
-    func loginWithGithub() {
-        Auth0
-            .webAuth()
-            .connection("github")
-            .start { result in
-                switch result {
-                case .success(let credentials):
-                    //                    print(credentials.idToken)
-                    decodeUserData(idToken: credentials.idToken)
-                case .failure(let error):
-                    print("Failed with : \(error)")
-                }
-            }
-    }
-    func loginWithGoogle() {
-        Auth0
-            .webAuth()
-            .connection("google-oauth2")
-            .start { result in
-                switch result {
-                case .success(let credentials):
-                    //                    print(credentials.idToken)
-                    decodeUserData(idToken: credentials.idToken)
-                case .failure(let error):
-                    print("Failed with : \(error)")
-                }
-            }
-    }
+//    func loginWithGithub() {
+//        Auth0
+//            .webAuth()
+//            .connection("github")
+//            .start { result in
+//                switch result {
+//                case .success(let credentials):
+//                    //                    print(credentials.idToken)
+//                    decodeUserData(idToken: credentials.idToken)
+//                case .failure(let error):
+//                    print("Failed with : \(error)")
+//                }
+//            }
+//    }
+//    func loginWithGoogle() {
+//        Auth0
+//            .webAuth()
+//            .connection("google-oauth2")
+//            .start { result in
+//                switch result {
+//                case .success(let credentials):
+//                    //                    print(credentials.idToken)
+//                    decodeUserData(idToken: credentials.idToken)
+//                case .failure(let error):
+//                    print("Failed with : \(error)")
+//                }
+//            }
+//    }
     
     func login() {
         Auth0
